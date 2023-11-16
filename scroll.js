112a7859ae1d72082f22556145f13f01e876c416
JuliaTarasenko00 <juliatarase17@gmail.com>
2023-11-16 22:57:07 +0200

    fix

--- scroll.js
+++ scroll.js
@@ -8,7 +8,7 @@ let cardPerView = Math.random(card.offsetWidth / firstCardWidth);
 
 const button_left = document.createElement('button');
 button_left.type = 'button';
-button_left.className = 'button_arrow_left';
+button_left.className = 'button_arrow_left button';
 
 const button_right = document.createElement('button');
 button_right.type = 'button';
@@ -20,6 +20,9 @@ carousel.append(button_right);
 carousel.addEventListener('click', onClickCarousel);
 card.addEventListener('scroll', scrollCard);
 
+const buttonLeft = document.querySelector('.button_arrow_left');
+const buttonRight = document.querySelector('.button_arrow_right');
+
 function onClickCarousel(ev) {
   const element = ev.target;
 
@@ -27,6 +30,17 @@ function onClickCarousel(ev) {
     return (card.scrollLeft -= firstCardWidth + 20);
   }
   if (element.classList.contains('button_arrow_right')) {
+    buttonLeft.removeAttribute('disabled');
+    buttonLeft.classList.remove('button');
     return (card.scrollLeft += firstCardWidth + 20);
+  }
+}
+
+function scrollCard() {
+  const length = card.scrollWidth - card.offsetWidth;
+  if (card.scrollLeft === 0) {
+    buttonLeft.classList.toggle('button');
+  } else if (Math.ceil(card.scrollLeft) === length) {
+    buttonRight.classList.toggle('button');
   }
 }
